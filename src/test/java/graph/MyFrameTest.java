64b996b4bd6a98588a8bc793b67c83a25c368bea
AlexNote <sashaorlov.ru@gmail.com>
2018-06-06 14:40:11 +0300

    travis

--- src/test/java/graph/MyFrameTest.java
+++ src/test/java/graph/MyFrameTest.java
@@ -44,126 +44,126 @@ public class MyFrameTest {
         window.button("buttonClear").click();
     }
 
-//    @Test
-//    public void checkTextFieldFinish() {
-//        window.textBox("textFieldFinish").enterText("");
-//        window.button("buttonFindWay").click();
-//        window.textBox("textFieldWay").requireText("Ошибка! Заполните все поля ввода");
-//        window.button("buttonClear").click();
-//
-//        window.textBox("textFieldFinish").enterText("asdf");
-//        window.button("buttonFindWay").click();
-//        window.textBox("textFieldWay").requireText("Ошибка! Заполните все поля ввода");
-//        window.button("buttonClear").click();
-//
-//        window.textBox("textFieldFinish").enterText("5");
-//        window.button("buttonFindWay").click();
-//        window.textBox("textFieldWay").requireText("Ошибка! Заполните все поля ввода");
-//        window.button("buttonClear").click();
-//
-//        window.textBox("textAreaMatrix").enterText("0 1 1 1 0\n1 0 0 1 0\n1 0 0 0 0\n1 1 0 0 1\n0 0 0 1 0");
-//        window.textBox("textFieldStart").enterText("0");
-//        window.textBox("textFieldFinish").enterText("4 d");
-//        window.button("buttonFindWay").click();
-//        window.textBox("textFieldWay").requireText("Ошибка! Некорректные вершины");
-//        window.button("buttonClear").click();
-//    }
-//
-//    @Test
-//    public void checkButtonClear() {
-//        window.textBox("textFieldStart").enterText("asdf");
-//        window.textBox("textFieldFinish").enterText("gweg ");
-//        window.textBox("textAreaMatrix").enterText("0 1 0\n1 0 0\n1 1 0");
-//        window.button("buttonClear").click();
-//        window.textBox("textFieldWay").requireText("");
-//        window.textBox("textAreaMatrix").requireText("");
-//        window.textBox("textFieldStart").requireText("");
-//        window.textBox("textFieldFinish").requireText("");
-//
-//        window.textBox("textAreaMatrix").enterText("0 1 1 1 0\n1 0 0 1 0\n1 0 0 0 0\n1 1 0 0 1\n0 0 0 1 0");
-//        window.textBox("textFieldStart").enterText("0");
-//        window.textBox("textFieldFinish").enterText("4");
-//        window.button("buttonClear").click();
-//        window.textBox("textFieldWay").requireText("");
-//        window.textBox("textAreaMatrix").requireText("");
-//        window.textBox("textFieldStart").requireText("");
-//        window.textBox("textFieldFinish").requireText("");
-//    }
-//
-//    @Test
-//    public void correctData() {
-//        window.textBox("textAreaMatrix").enterText("0 1 1 1 0\n1 0 0 1 0\n1 0 0 0 0\n1 1 0 0 1\n0 0 0 1 0");
-//        window.textBox("textFieldStart").enterText("0");
-//        window.textBox("textFieldFinish").enterText("4");
-//        window.button("buttonFindWay").click();
-//        window.textBox("textFieldWay").requireText("0 1 3 4");
-//        window.button("buttonClear").click();
-//        window.textBox("textFieldWay").requireText("");
-//        window.textBox("textAreaMatrix").requireText("");
-//        window.textBox("textFieldStart").requireText("");
-//        window.textBox("textFieldFinish").requireText("");
-//
-//        window.textBox("textAreaMatrix").enterText("0 1 1 1 0 0\n1 0 0 0 0 0\n1 0 0 0 1 0\n1 0 0 0 0 1\n0 0 1 0 0 0\n0 0 0 1 0 0");
-//        window.textBox("textFieldStart").enterText("0");
-//        window.textBox("textFieldFinish").enterText("5");
-//        window.button("buttonFindWay").click();
-//        window.textBox("textFieldWay").requireText("0 3 5");
-//        window.button("buttonClear").click();
-//        window.textBox("textFieldWay").requireText("");
-//        window.textBox("textAreaMatrix").requireText("");
-//        window.textBox("textFieldStart").requireText("");
-//        window.textBox("textFieldFinish").requireText("");
-//
-//        window.textBox("textAreaMatrix").enterText("0 1 1 1 0 0\n1 0 0 0 0 0\n1 0 0 0 1 0\n1 0 0 0 0 1\n0 0 1 0 0 0\n0 0 0 1 0 0");
-//        window.textBox("textFieldStart").enterText("4");
-//        window.textBox("textFieldFinish").enterText("6");
-//        window.button("buttonFindWay").click();
-//        window.textBox("textFieldWay").requireText("No way out.");
-//        window.button("buttonClear").click();
-//        window.textBox("textFieldWay").requireText("");
-//        window.textBox("textAreaMatrix").requireText("");
-//        window.textBox("textFieldStart").requireText("");
-//        window.textBox("textFieldFinish").requireText("");
-//
-//        window.textBox("textAreaMatrix").enterText("1 0\n0 1");
-//        window.textBox("textFieldStart").enterText("0");
-//        window.textBox("textFieldFinish").enterText("1");
-//        window.button("buttonFindWay").click();
-//        window.textBox("textFieldWay").requireText("No way out.");
-//        window.button("buttonClear").click();
-//        window.textBox("textFieldWay").requireText("");
-//        window.textBox("textAreaMatrix").requireText("");
-//        window.textBox("textFieldStart").requireText("");
-//        window.textBox("textFieldFinish").requireText("");
-//    }
-//
-//    @Test
-//    public void doubleClickButtonFindWay() {
-//        window.textBox("textAreaMatrix").enterText("0 1 1 1 0\n1 0 0 1 0\n1 0 0 0 0\n1 1 0 0 1\n0 0 0 1 0");
-//        window.textBox("textFieldStart").enterText("0");
-//        window.textBox("textFieldFinish").enterText("4");
-//        window.button("buttonFindWay").doubleClick();
-//        window.textBox("textFieldWay").requireText("0 1 3 4");
-//        window.button("buttonClear").click();
-//        window.textBox("textFieldWay").requireText("");
-//        window.textBox("textAreaMatrix").requireText("");
-//        window.textBox("textFieldStart").requireText("");
-//        window.textBox("textFieldFinish").requireText("");
-//    }
-//
-//    @Test
-//    public void textAreaCheck() {
-//        window.textBox("textAreaMatrix").enterText("bad Data");
-//        window.textBox("textFieldStart").enterText("0");
-//        window.textBox("textFieldFinish").enterText("4");
-//        window.button("buttonFindWay").click();
-//        window.textBox("textFieldWay").requireText("");
-//        window.button("buttonClear").click();
-//        window.textBox("textFieldWay").requireText("");
-//        window.textBox("textAreaMatrix").requireText("");
-//        window.textBox("textFieldStart").requireText("");
-//        window.textBox("textFieldFinish").requireText("");
-//    }
+    @Test
+    public void checkTextFieldFinish() {
+        window.textBox("textFieldFinish").enterText("");
+        window.button("buttonFindWay").click();
+        window.textBox("textFieldWay").requireText("Ошибка! Заполните все поля ввода");
+        window.button("buttonClear").click();
+
+        window.textBox("textFieldFinish").enterText("asdf");
+        window.button("buttonFindWay").click();
+        window.textBox("textFieldWay").requireText("Ошибка! Заполните все поля ввода");
+        window.button("buttonClear").click();
+
+        window.textBox("textFieldFinish").enterText("5");
+        window.button("buttonFindWay").click();
+        window.textBox("textFieldWay").requireText("Ошибка! Заполните все поля ввода");
+        window.button("buttonClear").click();
+
+        window.textBox("textAreaMatrix").enterText("0 1 1 1 0\n1 0 0 1 0\n1 0 0 0 0\n1 1 0 0 1\n0 0 0 1 0");
+        window.textBox("textFieldStart").enterText("0");
+        window.textBox("textFieldFinish").enterText("4 d");
+        window.button("buttonFindWay").click();
+        window.textBox("textFieldWay").requireText("Ошибка! Некорректные вершины");
+        window.button("buttonClear").click();
+    }
+
+    @Test
+    public void checkButtonClear() {
+        window.textBox("textFieldStart").enterText("asdf");
+        window.textBox("textFieldFinish").enterText("gweg ");
+        window.textBox("textAreaMatrix").enterText("0 1 0\n1 0 0\n1 1 0");
+        window.button("buttonClear").click();
+        window.textBox("textFieldWay").requireText("");
+        window.textBox("textAreaMatrix").requireText("");
+        window.textBox("textFieldStart").requireText("");
+        window.textBox("textFieldFinish").requireText("");
+
+        window.textBox("textAreaMatrix").enterText("0 1 1 1 0\n1 0 0 1 0\n1 0 0 0 0\n1 1 0 0 1\n0 0 0 1 0");
+        window.textBox("textFieldStart").enterText("0");
+        window.textBox("textFieldFinish").enterText("4");
+        window.button("buttonClear").click();
+        window.textBox("textFieldWay").requireText("");
+        window.textBox("textAreaMatrix").requireText("");
+        window.textBox("textFieldStart").requireText("");
+        window.textBox("textFieldFinish").requireText("");
+    }
+
+    @Test
+    public void correctData() {
+        window.textBox("textAreaMatrix").enterText("0 1 1 1 0\n1 0 0 1 0\n1 0 0 0 0\n1 1 0 0 1\n0 0 0 1 0");
+        window.textBox("textFieldStart").enterText("0");
+        window.textBox("textFieldFinish").enterText("4");
+        window.button("buttonFindWay").click();
+        window.textBox("textFieldWay").requireText("0 1 3 4");
+        window.button("buttonClear").click();
+        window.textBox("textFieldWay").requireText("");
+        window.textBox("textAreaMatrix").requireText("");
+        window.textBox("textFieldStart").requireText("");
+        window.textBox("textFieldFinish").requireText("");
+
+        window.textBox("textAreaMatrix").enterText("0 1 1 1 0 0\n1 0 0 0 0 0\n1 0 0 0 1 0\n1 0 0 0 0 1\n0 0 1 0 0 0\n0 0 0 1 0 0");
+        window.textBox("textFieldStart").enterText("0");
+        window.textBox("textFieldFinish").enterText("5");
+        window.button("buttonFindWay").click();
+        window.textBox("textFieldWay").requireText("0 3 5");
+        window.button("buttonClear").click();
+        window.textBox("textFieldWay").requireText("");
+        window.textBox("textAreaMatrix").requireText("");
+        window.textBox("textFieldStart").requireText("");
+        window.textBox("textFieldFinish").requireText("");
+
+        window.textBox("textAreaMatrix").enterText("0 1 1 1 0 0\n1 0 0 0 0 0\n1 0 0 0 1 0\n1 0 0 0 0 1\n0 0 1 0 0 0\n0 0 0 1 0 0");
+        window.textBox("textFieldStart").enterText("4");
+        window.textBox("textFieldFinish").enterText("6");
+        window.button("buttonFindWay").click();
+        window.textBox("textFieldWay").requireText("No way out.");
+        window.button("buttonClear").click();
+        window.textBox("textFieldWay").requireText("");
+        window.textBox("textAreaMatrix").requireText("");
+        window.textBox("textFieldStart").requireText("");
+        window.textBox("textFieldFinish").requireText("");
+
+        window.textBox("textAreaMatrix").enterText("1 0\n0 1");
+        window.textBox("textFieldStart").enterText("0");
+        window.textBox("textFieldFinish").enterText("1");
+        window.button("buttonFindWay").click();
+        window.textBox("textFieldWay").requireText("No way out.");
+        window.button("buttonClear").click();
+        window.textBox("textFieldWay").requireText("");
+        window.textBox("textAreaMatrix").requireText("");
+        window.textBox("textFieldStart").requireText("");
+        window.textBox("textFieldFinish").requireText("");
+    }
+
+    @Test
+    public void doubleClickButtonFindWay() {
+        window.textBox("textAreaMatrix").enterText("0 1 1 1 0\n1 0 0 1 0\n1 0 0 0 0\n1 1 0 0 1\n0 0 0 1 0");
+        window.textBox("textFieldStart").enterText("0");
+        window.textBox("textFieldFinish").enterText("4");
+        window.button("buttonFindWay").doubleClick();
+        window.textBox("textFieldWay").requireText("0 1 3 4");
+        window.button("buttonClear").click();
+        window.textBox("textFieldWay").requireText("");
+        window.textBox("textAreaMatrix").requireText("");
+        window.textBox("textFieldStart").requireText("");
+        window.textBox("textFieldFinish").requireText("");
+    }
+
+    @Test
+    public void textAreaCheck() {
+        window.textBox("textAreaMatrix").enterText("bad Data");
+        window.textBox("textFieldStart").enterText("0");
+        window.textBox("textFieldFinish").enterText("4");
+        window.button("buttonFindWay").click();
+        window.textBox("textFieldWay").requireText("");
+        window.button("buttonClear").click();
+        window.textBox("textFieldWay").requireText("");
+        window.textBox("textAreaMatrix").requireText("");
+        window.textBox("textFieldStart").requireText("");
+        window.textBox("textFieldFinish").requireText("");
+    }
 
     @After
     public void tearDown() {
